aed202269c1f1ddc1acfa2882e7834fcfce50200
Falsin <greenldend@gmail.com>
2022-02-06 22:18:50 +0700

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -16,7 +16,11 @@ var app = express();
 app.use(helmet());
 
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://m001-student:m001-mongodb-basics@sandbox.jnrzi.mongodb.net/local_library?retryWrites=true&w=majority';
+//var mongoDB = 'mongodb+srv://m001-student:m001-mongodb-basics@sandbox.jnrzi.mongodb.net/local_library?retryWrites=true&w=majority';
+
+var dev_db_url = 'mongodb+srv://m001-student:m001-mongodb-basics@sandbox.jnrzi.mongodb.net/local_library?retryWrites=true'
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
+
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
